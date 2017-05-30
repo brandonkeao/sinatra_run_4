c2066bbec610c1a79db38a488ff0d6f59c41039d
brandonkeao <brandon.keao@gmail.com>
2017-05-29 18:33:35 -0600

    Added basic routes and access params for output strings.

--- basics.rb
+++ basics.rb
@@ -4,3 +4,18 @@ get '/' do
   "Hello, World!"
 end
 
+get '/about' do
+  'A little about me.'
+end
+
+get '/hello/:name' do
+  "Hello there, #{params[:name].upcase}."
+end
+
+get '/hello/:name/:city' do
+  "Hey there #{params[:name]} from #{params[:city]}."
+end
+
+get '/more/*' do
+  params[:splat]
+end
